7baa8120c8d399df11493095ffefce4abae6b94b
redrum <urecherrydream@gmail.com>
2022-08-22 01:46:42 +0300

    1st homewoerk is done

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
-    return (
-        <div>
+type MessagePropsType = {
+  avatar: string
+  name: string
+  message: string
+  time: string
+}
 
-        </div>
-    )
+function Message(props: MessagePropsType) {
+  return (
+    <div className={s.container}>
+      <div>
+        <img className={s.container} src={props.avatar} alt='avatar' />
+      </div>
+      <div className={s.box}>
+        <h2>{props.name}</h2>
+        <p>{props.message}</p>
+        <p className={s.time}>{props.time}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Message
